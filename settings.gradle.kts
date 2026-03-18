0d8ccd533c7f11481a8d8c0ca12050ad3a9ce2fe
Pramod Narnolia <narnoliapramod561@gmail.com>
2026-03-19 01:15:46 +0530

    Sync repository: include user's manual Gradle updates and architecture fixes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,23 @@
 pluginManagement {
-    repositories { google(); mavenCentral(); gradlePluginPortal() }
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
     plugins {
         id("com.android.application") version "9.1.0"
-        id("org.jetbrains.kotlin.android") version "2.2.10"
+        id("org.jetbrains.kotlin.android") version "2.1.0"
         id("com.google.dagger.hilt.android") version "2.51"
-        id("com.google.devtools.ksp") version "2.3.2"
-        id("org.jetbrains.kotlin.plugin.compose") version "2.2.10"
+        id("com.google.devtools.ksp") version "2.1.0-1.0.29"
+        id("org.jetbrains.kotlin.plugin.compose") version "2.1.0"
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories { google(); mavenCentral() }
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 rootProject.name = "CampusLink"
 include(":app")
